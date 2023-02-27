80f166c686a5b74dd1eb39efd94ae11e13f7351e
sanjeev-rm <sanjeevraghu2050@gmail.com>
2023-02-28 00:01:28 +0530

    Initial Commit

---
+++ OpenAI GPT3/OpenAI_GPT3App.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAI_GPT3App.swift
+//  OpenAI GPT3
+//
+//  Created by Sanjeev RM on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenAI_GPT3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
